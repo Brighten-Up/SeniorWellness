993471d3960cb90f38d1c7d20fb63b0f481bdb4b
Virtus_Chae <ybd4731@gmail.com>
2024-08-15 13:31:06 +0900

    first commit

---
+++ src/main/java/com/brightenup/seniorwellness/SeniorWellnessApplication.java
@@ -0,0 +1,13 @@
+package com.brightenup.seniorwellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorWellnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorWellnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brightenup/seniorwellness/SeniorWellnessApplicationTests.java
@@ -0,0 +1,14 @@
+package com.brightenup.seniorwellness;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorWellnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow();
+    }
+}
